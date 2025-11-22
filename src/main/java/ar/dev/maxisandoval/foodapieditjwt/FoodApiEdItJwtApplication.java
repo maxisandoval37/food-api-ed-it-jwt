da4acc31a0a16b4010b08615758486fbe8e7f2c5
Maxi Sandoval <maxisandoval98@gmail.com>
2025-11-22 12:23:29 -0300

    first commit (base)

---
+++ src/main/java/ar/dev/maxisandoval/foodapieditjwt/FoodApiEdItJwtApplication.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.foodapieditjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApiEdItJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodApiEdItJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/dev/maxisandoval/foodapieditjwt/FoodApiEdItJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.foodapieditjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApiEdItJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
