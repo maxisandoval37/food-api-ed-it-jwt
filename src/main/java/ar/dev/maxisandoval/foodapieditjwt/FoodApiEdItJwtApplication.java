fcba1ee05b9bf8c427069bfaadc865e32abc4fc2
Maxi Sandoval <maxisandoval98@gmail.com>
2025-11-22 13:05:02 -0300

    entidades jpa

--- src/main/java/ar/dev/maxisandoval/foodapieditjwt/FoodApiEdItJwtApplication.java
+++ src/main/java/ar/dev/maxisandoval/foodapieditjwt/FoodApiEdItJwtApplication.java
@@ -1,7 +1,5 @@
 package ar.dev.maxisandoval.foodapieditjwt;
 
-import ar.dev.maxisandoval.foodapieditjwt.entity.Food;
-import ar.dev.maxisandoval.foodapieditjwt.entity.Ingredient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,13 +8,6 @@ public class FoodApiEdItJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FoodApiEdItJwtApplication.class, args);
-
-        Food food = new Food(1L, "Asado", 12);
-
-        Ingredient ingredient = new Ingredient();
-
-        ingredient.getFood();
-
     }
 
 }
--- src/main/java/ar/dev/maxisandoval/foodapieditjwt/entity/Food.java
+++ src/main/java/ar/dev/maxisandoval/foodapieditjwt/entity/Food.java
@@ -1,12 +1,16 @@
 package ar.dev.maxisandoval.foodapieditjwt.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Food {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
