7b0ca1b0724f13cc0292c9c439d203184bcbc764
Maxi Sandoval <maxisandoval98@gmail.com>
2025-12-06 12:57:54 -0300

    se agrega cache a la app

--- src/main/java/ar/dev/maxisandoval/foodapieditjwt/FoodApiEdItJwtApplication.java
+++ src/main/java/ar/dev/maxisandoval/foodapieditjwt/FoodApiEdItJwtApplication.java
@@ -2,8 +2,10 @@ package ar.dev.maxisandoval.foodapieditjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FoodApiEdItJwtApplication {
 
     public static void main(String[] args) {
--- src/main/java/ar/dev/maxisandoval/foodapieditjwt/service/FoodService.java
+++ src/main/java/ar/dev/maxisandoval/foodapieditjwt/service/FoodService.java
@@ -3,6 +3,7 @@ package ar.dev.maxisandoval.foodapieditjwt.service;
 import ar.dev.maxisandoval.foodapieditjwt.entity.Food;
 import ar.dev.maxisandoval.foodapieditjwt.repository.FoodRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -12,18 +13,22 @@ public class FoodService {
 
     private final FoodRepository foodRepository;
 
+    @Cacheable("foods")
     public List<Food> getAll() {
         return foodRepository.findAll();
     }
 
+    @Cacheable(value = "food", key = "#id")
     public Food getById(Long id) {
         return foodRepository.findById(id).orElseThrow(() -> new RuntimeException("Food not found with id: "+id));
     }
 
+    @CachePut(value = "food", key = "#result.id")
     public Food save(Food food) {
         return foodRepository.save(food);
     }
 
+    @CachePut(value = "food", key = "#id")
     public Food update(Long id, Food updatedFood) {
         Food existingFood = getById(id);
         existingFood.setName(updatedFood.getName());
@@ -32,10 +37,12 @@ public class FoodService {
         return foodRepository.save(existingFood);
     }
 
-    public void deleteById(Long id){
+    @CacheEvict(value = "foods", allEntries = true)
+    public void deleteById(Long id) {
         foodRepository.deleteById(id);
     }
 
+    @CacheEvict(value = "foods", allEntries = true)
     public void deleteAll() {
         foodRepository.deleteAll();
     }
