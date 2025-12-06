c35eb0a75a8e5984c91bb881d27ab5daa36345ef
Maxi Sandoval <maxisandoval98@gmail.com>
2025-12-06 12:36:44 -0300

    se agregan cors

---
+++ src/main/java/ar/dev/maxisandoval/foodapieditjwt/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package ar.dev.maxisandoval.foodapieditjwt.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/***", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
